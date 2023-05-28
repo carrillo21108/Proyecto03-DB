6889817b0763a18f0d97d88fa0662360ca5c6f64
carrillo21108 <87512111+carrillo21108@users.noreply.github.com>
2023-05-27 19:42:43 -0600

    Tabla Products y Channels

--- datamart.sql
+++ datamart.sql
@@ -1,1 +1,37 @@
 CREATE DATABASE datamart_proyecto03;
+
+CREATE TABLE sales(
+    product_id INT, //FK
+    channel_id INT, //FK
+);
+
+CREATE TABLE products(
+    product_id INT, //PK
+    name VARCHAR(46),
+    product_subcategory VARCHAR(50),
+    product_category VARCHAR(50),
+    list_price VARCHAR(10),
+    min_price VARCHAR(9),
+
+    PRIMARY KEY(product_id)
+);
+
+CREATE TABLE channels(
+    channel_id INT, PK
+    class VARCHAR(20),
+    name VARCHAR(20),
+
+    PRIMARY KEY(channel_id)
+);
+
+CREATE TABLE promotions(
+
+);
+
+CREATE TABLE times(
+
+);
+
+CREATE TABLE geography(
+
+);
